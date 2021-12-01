e9aca1939c1fd7c34de33e1444d95e19e28653bb
Bradley Morck <bmorc001@sledge.cs.ucr.edu>
2021-11-30 21:02:43 -0800

    Believe the open function works

--- shm.c
+++ shm.c
@@ -7,6 +7,8 @@
 #include "proc.h"
 #include "spinlock.h"
 
+#define base (KERNBASE - (64 * PGSIZE))
+
 struct {
   struct spinlock lock;
   struct shm_page {
@@ -31,10 +33,41 @@ void shminit() {
 int shm_open(int id, char **pointer) {
 
 //you write this
+  int i;
+  struct proc *curproc = myproc();
+  //struct shm_page mypage;
+  acquire(&(shm_table.lock));
+  //case 1
+  for (i = 0; i< 64; i++) {
+    if(shm_table.shm_pages[i].id == id){
+      //found page since id exists
 
+      mappages(curproc->pgdir, (void*)PGROUNDUP(curproc->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame),PTE_W|PTE_U);
+      *pointer = (char*)PGROUNDUP(curproc->sz);
+      curproc->sz += PGSIZE;
+      //increment ref
+      shm_table.shm_pages[i].refcnt +=1;
+    }
+  }
+  //case 2
+  for (i = 0; i< 64; i++) {
+    if(shm_table.shm_pages[i].id == 0){
+      //empty entry, so we can alloc
+      shm_table.shm_pages[i].frame =kalloc();
+      if(!(shm_table.shm_pages[i].frame)){
+        cprintf("ERROR, pg could not be alloced");
+        return 0;
+      }
+      memset(shm_table.shm_pages[i].frame,0,PGSIZE);
+      shm_table.shm_pages[i].id = id;
+      shm_table.shm_pages[i].refcnt +=1;
+      mappages(curproc->pgdir, (void*)PGROUNDUP(curproc->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame),PTE_W|PTE_U);
+      *pointer = (char*)PGROUNDUP(curproc->sz);
+      curproc->sz += PGSIZE;
+    }
+  }
 
-
-
+  release(&(shm_table.lock));
 return 0; //added to remove compiler warning -- you should decide what to return
 }
 
